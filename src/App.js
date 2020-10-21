8c6f858b3f8ac63dc7e01c3aefb947466bd2a83c
LeeEugene1 <uwm1004@gmail.com>
2020-10-21 22:45:14 +0900

    createCustomer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import React from 'react';
 import './App.css';
+import Customer from './components/Customer';
 
+const customer = [
+  {
+  'id':1,
+  'image':'https://placeimg.com/64/64/any',
+  'name':'유딩딩',
+  'birthday':'920921',
+  'gender':'여성',
+  'job':'개발자'
+
+},
+{
+  'id':2,
+  'image':'https://placeimg.com/64/64/any',
+  'name':'유밍밍',
+  'birthday':'950714',
+  'gender':'여성',
+  'job':'카페사장'
+
+}
+]
 function App() {
   return (
-    <div className="gray-background">
-      <h2>management system</h2>
-    </div>
+      // <Customer
+      //   id={customer[0].id}
+      //   image={customer[0].image}
+      //   name={customer[0].name}
+      //   birthday={customer[0].birthday}
+      //   gender={customer[0].gender}
+      //   job={customer[0].job}
+      // />
+      <>
+        {customer.map(c =>{
+          return(
+            <Customer
+              key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+
+          );
+        })}
+      </>
   );
 }
 
